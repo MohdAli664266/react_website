325e01ad00b318137422bc8c607264d35724c336
MohdAli664266 <moaliansari664266@gmail.com>
2024-01-20 23:26:16 +0530

    SITE IS WORKING

--- src/Components/GlobalApi.js
+++ src/Components/GlobalApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const movieBase = "https://api.themoviedb.org/3";
 const api_key = "27a23be4ff99daa45981e8e14fcbf888";
-const getTraindingMovies = axios.get(movieBase+"/trending/all/day?api_key="+api_key);
+const getTraindingMovies = axios.get(movieBase+"/movie/top_rated?api_key="+api_key);
 // 'https://api.themoviedb.org/3/movie/top_rated?api_key=27a23be4ff99daa45981e8e14fcbf888'
 export default {
     getTraindingMovies
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,36 +12,28 @@ function Home() {
 
   const getTraindingMovie = ()=>{
     GlobalApi.getTraindingMovies.then((res)=>{
-      console.log(res.data.results);
       setMovie(res.data.results);
     })
   }
 const slideRight = (element)=>
 {
-  element.scrollLeft += windth-110;
+  element.scrollLeft += windth-60;
 }
 const slideLeft = (element)=>
 {
-  element.scrollLeft -= windth-110;
+  element.scrollLeft -= windth-60;
 }
   return (
     <div>
-      <HiChevronLeft className='cursor-pointer hidden md:block text-white text-4xl md:mx-8 absolute md:mt-[225px]' onClick={()=>slideLeft(elementRef.current)}/>
-      <HiChevronRight className='cursor-pointer hidden md:block text-white text-4xl md:mx-8 absolute md:mt-[225px] right-0' onClick={()=>slideRight(elementRef.current)} />
-      <div className='flex overflow-x-auto w-full px-16 py-2 my-4 scrollbar-none scroll-smooth' ref={elementRef}>
+      <HiChevronLeft className='cursor-pointer hidden md:block text-amber-800 text-4xl md:mx-3 absolute md:mt-[225px]' onClick={()=>slideLeft(elementRef.current)}/>
+      <HiChevronRight className='cursor-pointer hidden md:block text-amber-800 text-4xl md:mx-3 absolute md:mt-[225px] right-0' onClick={()=>slideRight(elementRef.current)} />
+      <div className='flex overflow-x-auto w-full px-10 py-2 my-4 scrollbar-none scroll-smooth' ref={elementRef}>
         {movie.map((data)=>
         (
           <img className='min-w-full md:h-[450px] h-[190px] object-cover shadow-lg shadow-slate-900 mr-5 rounded-md hover:border-2 border-gray-400 transition-all duration-100 ease-in object-left-top' src={baseUrl+data.backdrop_path} alt="" />
         ))}
       </div>
     </div>
-    // <>
-    //   <div className='bg-[hsl(120,2%,26%)] h-screen w-fit flex justify-center items-center text-xl '>
-    //     {dataContainer.map((data)=>(
-    //       <SliderImage image={data.image} imageName = {data.imageName} />
-    //     ))}
-    //   </div>
-    // </>
   )
 }
 
