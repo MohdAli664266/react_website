7d2ee6c4b94258910fb46b0676c3eafa731c26f3
MohdAli664266 <moaliansari664266@gmail.com>
2024-01-22 02:43:46 +0530

    About section is added successfully

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
 function About() {
   return (
-    <div className='bg-[rgb(0,255,0)] flex justify-center items-center text-xl'>About</div>
+    <>
+      <div className='min-w-full flex justify-center items-center px-10 mt-4 gap-4'>
+
+        <div className='min-w-[20%] h-full flex-row justify-center items-center '>
+          <div className='min-w-[100px] max-w-[160px] flex justify-center items-center'>
+            <img src="https://www.freepnglogos.com/uploads/logo-home-png/pros-cons-investing-rental-property-piktochart-14.png" alt="" srcset="" />
+          </div>
+          
+          <div className=' flex flex-col p-4 gap-4 text-2xl font-bold text-purple-700'>
+            <h3 className=' border-gray-900 shadow-lg hover:text-purple-400 shadow-gray-950 px-2 cursor-pointer'>About</h3>
+            <h3 className=' border-gray-900 shadow-lg hover:text-purple-400 shadow-gray-950 px-2 cursor-pointer'>Skills</h3>
+            <h3 className=' border-gray-900 shadow-lg hover:text-purple-400 shadow-gray-950 px-2 cursor-pointer'>Projects</h3>
+            <h3 className=' border-gray-900 shadow-lg hover:text-purple-400 shadow-gray-950 px-2 cursor-pointer'>Goal</h3>
+            <h3 className=' border-gray-900 shadow-lg hover:text-purple-400 shadow-gray-950 px-2 cursor-pointer'>Hobbies</h3>
+            
+          </div>
+        </div>
+
+        <div className='min-w-[80%] min-h-full flex flex-col justify-center items-center p-4 gap-4'>
+          <h1 className='shadow-lg shadow-gray-900 text-2xl p-2 font-bold'>Heading will display here</h1>
+
+          <div className='flex'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sit minus debitis ad labore odio dolore suscipit sequi soluta, porro libero. Voluptatibus tempora deleniti eligendi aspernatur officia reprehenderit facilis adipisci?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, laborum expedita itaque error sit tempora debitis voluptas, fugit ipsum odio harum pariatur vitae blanditiis, libero necessitatibus officiis aperiam? Obcaecati, quaerat.
+            </p>
+            <img className='max-w-[250px]' src="https://www.freepnglogos.com/uploads/logo-home-png/pros-cons-investing-rental-property-piktochart-14.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Components/Service.js
+++ src/Components/Service.js
@@ -1,12 +1,90 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from 'react'
 
 function Service() {
-  return (
-    <>
-        <div className='bg-[rgb(0,255,0)] flex justify-center items-center text-xl'>Service</div>
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(6);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const passwordRef = useRef(null);
+  
+  const passwordGenerator = ()=>{
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let pass = '';
+    if(numberAllowed) str += "1234567890";
+    if(charAllowed) str += "@~`!#$%^&*()_+;:/?,{}";
+    
+    for (let index = 0; index <length; index++) {
+      const char = Math.floor(Math.random() * str.length +1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass)
+  }
 
-    </>
-  );
+  useEffect(()=>{
+    passwordGenerator();
+  },[length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    <div className='flex justify-center items-center max-w-full h-screen bg-[hsl(120,2%,26%)]'>
+      <div className=" flex-col min-w-60 bg-gray-800 shadow-lg shadow-slate-900 text-white px-4 ">
+
+        <div className="text-lg md:text-2xl flex items-center justify-center">
+          <h1 className='p-8 bold font-bold'>Password Generator</h1>
+        </div>
+
+        <div className='flex shadow-lg shadow-slate-900 rounded-lg overflow-hidden mb-4'>
+          <input 
+          type="text"
+          className="outline-none w-full py-1 px-3 text-yellow-900"
+          placeholder='Password'
+          value={password}
+          readOnly
+          ref={passwordRef}
+          />
+
+          <button
+          className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          onClick={()=>{
+            passwordRef.current?.select();
+            // passwordRef.current?.setSelectionRange(0, 6);
+            window.navigator.clipboard.writeText(password);
+          }}
+          >copy</button>
+        </div>
+
+        <div className='md:flex items-center gap-3 py-2'>
+          <div className='flex items-center justify-center'>
+            <input 
+            type="range"
+            min={6}
+            max={50}
+            value={length}
+            onChange={(e)=>setLength(e.target.value)}
+            />
+            <label className='font-bold text-yellow-400 px-2'>Length: {length}</label>
+          </div>
+
+          <div className='flex gap-2'>
+            <input 
+            type="checkbox"
+            value={length}
+            onClick={()=>setNumberAllowed(!numberAllowed)}
+            />
+            <label className='font-bold text-rose-600'>NumberAllowed</label>
+          </div>
+
+          <div  className='flex gap-2'>
+            <input 
+            type="checkbox"
+            value={length}
+            onClick={()=>setCharAllowed(!charAllowed)}
+            />
+            <label className='font-bold text-fuchsia-600'>CharacterAllowed</label>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Service;
+export default Service
