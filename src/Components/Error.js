cf92f03e5579d43f7437d1736957b6f4dcd2a63a
MohdAli664266 <moaliansari664266@gmail.com>
2024-01-23 15:46:50 +0530

    Error page fixed

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 function Error() {
   return (
     <div className='flex justify-center items-center text-xl'>
-      <img className='max-w-[800px]' src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg" alt="" srcset="" />
+      {/* <img className='max-w-[800px]' src="https://colorlib.com/wp/wp-content/uploads/sites/2/404-error-page-templates.jpg" alt="" srcset="" />
+       */}
+       <div className='bg-gray-800 flex justify-center items-center text-gray-400 flex-col max-w-[60%] my-8 p-8 shadow-lg shadow-slate-950'>
+        <h1 className='md:text-9xl text-5xl bg-transparent py-4 text-white font-bold'>Oops!</h1>
+        <p className='text-purple-300 p-4 m-4'>We are sorry! This page does not exist</p>
+        <Link to = 'home'>
+          <button className='bg-gray-800 shadow-md shadow-gray-950 text-blue-800 hover:text-blue-700 px-4 py-0.5 font-bold mt-4'>Go to home</button>
+        </Link>
+       </div>
     </div>
   )
 }
